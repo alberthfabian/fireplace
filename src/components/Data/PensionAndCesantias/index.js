e632f87df593e6a1cec1c173a95ce21ae2dfa0c4
Alberth Perez <alberth.perez@hotmail.com>
2022-02-07 11:43:53 -0500

    Update colpensiones

--- src/components/Data/PensionAndCesantias/index.js
+++ src/components/Data/PensionAndCesantias/index.js
@@ -1,47 +1,52 @@
 export const pensionAndCesantias = [
   {
-    'id': 1,
-    'name': 'COLPENSIONES - RECIBO PARA PAGO',
-    'video': 'Video',
-    'linkVideo': 'https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g',
-    'linkDocument': 'https://sub.colpensionestransaccional.gov.co/Login_consultaFact.aspx',
-    'value': '1.000',
-    'register': 'No. de identificación'
+    id: 1,
+    name: "COLPENSIONES - RECIBO PARA PAGO",
+    video: "Video",
+    linkVideo: "https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g",
+    linkDocument:
+      "https://sub.colpensionestransaccional.gov.co/LoginDaMLayout.aspx?tagcliente=cup",
+    value: "1.000",
+    register: "No. de identificación y fecha de expedición",
   },
   {
-    'id': 2,
-    'name': 'FONDO NACIONAL DEL AHORRO',
-    'video': 'Video',
-    'linkVideo': 'https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g',
-    'linkDocument': 'https://www.fna.gov.co:8081/CTSProxy/services/cobis/ibweb/ib-container/index.html#/login',
-    'value': '1.000',
-    'register': 'Usuario y clave'
+    id: 2,
+    name: "FONDO NACIONAL DEL AHORRO",
+    video: "Video",
+    linkVideo: "https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g",
+    linkDocument:
+      "https://www.fna.gov.co:8081/CTSProxy/services/cobis/ibweb/ib-container/index.html#/login",
+    value: "1.000",
+    register: "Usuario y clave",
   },
   {
-    'id': 3,
-    'name': 'COLFONDOS',
-    'video': 'Video',
-    'linkVideo': 'https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g',
-    'linkDocument': 'https://www.colfondos.com.co/dxp/personas/pensiones-obligatorias/certificado-afiliacion',
-    'value': '1.000',
-    'register': 'No. de identificación'
+    id: 3,
+    name: "COLFONDOS",
+    video: "Video",
+    linkVideo: "https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g",
+    linkDocument:
+      "https://www.colfondos.com.co/dxp/personas/pensiones-obligatorias/certificado-afiliacion",
+    value: "1.000",
+    register: "No. de identificación",
   },
   {
-    'id': 4,
-    'name': 'PORVENIR',
-    'video': 'Video',
-    'linkVideo': 'https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g',
-    'linkDocument': 'https://www.porvenir.com.co/web/certificados-y-extractos/certificado-de-afiliacion',
-    'value': '2.000',
-    'register': 'No. de identificación'
+    id: 4,
+    name: "PORVENIR",
+    video: "Video",
+    linkVideo: "https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g",
+    linkDocument:
+      "https://www.porvenir.com.co/web/certificados-y-extractos/certificado-de-afiliacion",
+    value: "2.000",
+    register: "No. de identificación",
   },
   {
-    'id': 5,
-    'name': 'PROTECCIÓN',
-    'video': 'Video',
-    'linkVideo': 'https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g',
-    'linkDocument': 'https://www.proteccion.com/wps/portal/proteccion/web/home/home-afiliados-pensionados/certifacil#/certificados',
-    'value': '1.000',
-    'register': 'No. de identificación'
-  }
-]
+    id: 5,
+    name: "PROTECCIÓN",
+    video: "Video",
+    linkVideo: "https://www.youtube.com/channel/UCvi2KWTO8kKJZUO8cdJ2b6g",
+    linkDocument:
+      "https://www.proteccion.com/wps/portal/proteccion/web/home/home-afiliados-pensionados/certifacil#/certificados",
+    value: "1.000",
+    register: "No. de identificación",
+  },
+];
